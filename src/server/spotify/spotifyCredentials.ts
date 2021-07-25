89ca03897f34a8929090def4caec780b71b4da2a
Kevin Minutti <minutti70@gmail.com>
2021-07-25 15:31:01 -0400

    Update list of desired scopes for Spotify

--- src/server/spotify/spotifyCredentials.ts
+++ src/server/spotify/spotifyCredentials.ts
@@ -5,9 +5,24 @@ export type Credentials = {
   scopes: string;
 };
 
+const allScopes: string[] = [
+  "streaming",
+  "user-read-mail",
+  "user-read-private",
+  "user-library-read",
+  "user-top-read",
+  "user-read-recently-played",
+  "user-read-currently-playing",
+  "user-read-playback-position",
+  "user-read-playback-state",
+  "user-modify-playback-state",
+  "playlist-read-private",
+  "playlist-read-collaborative",
+];
+
 export const credentials: Credentials = {
   clientId: String(process.env.CLIENT_ID),
   clientSecret: String(process.env.CLIENT_SECRET),
   redirectURI: String(process.env.REDIRECT_URI),
-  scopes: "user-read-private user-read-email",
+  scopes: allScopes.join(" "),
 };
