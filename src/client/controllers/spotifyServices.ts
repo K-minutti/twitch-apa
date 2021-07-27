4083ef5c4cdb18966ff00eb81e274c162fe24dbb
Kevin Minutti <minutti70@gmail.com>
2021-07-27 09:34:26 -0400

    Update /api structure to match client route

--- src/client/controllers/spotifyServices.ts
+++ src/client/controllers/spotifyServices.ts
@@ -4,7 +4,7 @@ import { TracksResponse, EmptySearchResults } from "./types";
 export const getSpotifySearchResults = (
   query: string
 ): Promise<TracksResponse | EmptySearchResults> => {
-  return axios.get(`/api/search?q=${query}`).then((response) => {
+  return axios.get(`/api/search/${query}`).then((response) => {
     if (response.data.error) return { items: [] };
     return response.data["tracks"];
   });
