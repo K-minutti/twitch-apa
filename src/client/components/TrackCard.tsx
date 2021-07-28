29407852230620406725cf48b0eb3fec7309ce48
Kevin Minutti <minutti70@gmail.com>
2021-07-28 01:10:05 -0400

    Add Track Card component This is a UI component with one important feature mainly the play button that appears when a user hovers over an item indicates wether the track is playable or not the play button is disabled if its is not and enable if it is

--- src/client/components/TrackCard.tsx
+++ src/client/components/TrackCard.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
+import { PlayArrowRounded } from "@material-ui/icons/";
 import { useStyles } from "./styles/TrackCard.styles";
 import { Track } from "../controllers/types";
 
@@ -13,14 +14,33 @@ const TrackCard: React.FC<Props> = ({ track, setTrack }) => {
   const [showPlayIcon, setShowPlayIcon] = useState(false);
   const classes = useStyles();
 
+  const previewNullDisableButton: boolean = track.preview_url === null;
+
   return (
-    <Card className={classes.container} key={track.id}>
-      <img src={track.album.images[1].url} height="60px" width="60px" />
+    <Card
+      className={classes.container}
+      raised
+      onMouseEnter={() => setShowPlayIcon(true)}
+      onMouseLeave={() => setShowPlayIcon(false)}
+    >
+      <div className={classes.imageContainer}>
+        <img src={track.album.images[1].url} height="120px" width="120px" />
+      </div>
       <CardContent>
-        <h4>{track.name}</h4>
-        <h5>{track.artists[0].name}</h5>
-        {showPlayIcon && <button onClick={() => setTrack(track)}>Play</button>}
+        <h4 className={classes.title}>{track.name}</h4>
+        <h5 className={classes.name}>{track.artists[0].name}</h5>
       </CardContent>
+      {showPlayIcon && (
+        <button
+          className={classes.playButton}
+          onClick={() => setTrack(track)}
+          disabled={previewNullDisableButton}
+          onMouseLeave={() => setShowPlayIcon(false)}
+        >
+          <PlayArrowRounded className={classes.playIcon} />
+          Play
+        </button>
+      )}
     </Card>
   );
 };
--- src/client/components/styles/TrackCard.styles.tsx
+++ src/client/components/styles/TrackCard.styles.tsx
@@ -3,17 +3,54 @@ import { makeStyles, createStyles } from "@material-ui/core/styles";
 export const useStyles = makeStyles(() =>
   createStyles({
     container: {
-      height: "66px",
-      width: "360px",
+      height: "240px",
+      width: "220px",
       color: "#565656",
-      border: "solid #blue 1px",
-      borderRadius: "5px",
-      padding: "0px 4px",
+      textAlign: "center",
+      margin: "26px",
+      display: "flex",
+      position: "relative",
+      zIndex: 1,
+      flexDirection: "column",
+      alighItems: "center",
+      justifyContent: "center",
+      "&:hover": {
+        background: "#F4F4F4",
+      },
+    },
+    imageContainer: {
+      width: "100%",
+    },
+    title: {
+      color: "#212121",
+    },
+    name: {
+      color: "#565656",
+    },
+    playButton: {
+      backgroundColor: "#1EB980",
+      fontWeight: "bold",
+      color: "#fff",
+      height: "36px",
+      width: "88px",
       display: "flex",
       flexDirection: "row",
       alignItems: "center",
-      overflowX: "clip",
-      justifyContent: "flex-start",
+      justifyContent: "center",
+      borderRadius: "5px",
+      border: "none",
+      outline: "none",
+      position: "absolute",
+      right: "16px",
+      top: "50%",
+      zIndex: 2,
+      background: "#f1f1f1",
+      "&:disabled": {
+        backgroundColor: "#565656",
+      },
+    },
+    playIcon: {
+      fill: "#fff",
     },
   })
 );
